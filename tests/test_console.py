0e5e10b20c14f51c5d54162acbedd6dec411fdb3
Nsisong-Akpakpan <nsisongakpakpan01@gmail.com>
2022-11-27 01:39:00 -0800

    test_console.py file

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 26 22:15:09 2022
-@author: meco
+Created on Sat Nov  26 13:56:43 2022
+@author: Nsisong Akpakpan
+         Lebi shadrack
 """
 import sys
 import unittest
