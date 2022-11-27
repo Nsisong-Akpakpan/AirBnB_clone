df4bb65d9f1fc12d196d1c04b6cfb3967e86f0aa
Nsisong-Akpakpan <nsisongakpakpan01@gmail.com>
2022-11-27 01:13:31 -0800

    __init__.py file

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jun 27 13:56:43 2020
+Created on Sat Nov  26 13:56:43 2022
 
-@authors: Robinson Montes
-          Carlos Murcia
+@authors: Nsisong Akpakpan
+          Lebi shadrack
 """
 from models.engine.file_storage import FileStorage
 
