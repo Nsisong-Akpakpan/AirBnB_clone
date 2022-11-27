68f77f692afffa451b81261a8cee2c6e76a1e473
Nsisong-Akpakpan <nsisongakpakpan01@gmail.com>
2022-11-27 08:32:19 -0800

    deletes

--- models/engine/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov  26 13:56:43 2022
-
-@author: Nsisong Akpakpan
-         Lebi shadrack
-"""
--- tests/test_models/test_engine/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 26 23:02:17 2022
-@author: Lebi Shadrack
-         Nsisong Akpakpan
-"""
